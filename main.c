c00a947f352d4d6c70c46ae4780bd9dd18a16f63
mattgeroulo <mattgeroulo@yahoo.com>
2026-01-26 19:40:38 -0500

    shell takes input and outputs number of bytes read, exit by entering nothing or ^C

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 
-void readLine(){
+int readLine(){
     
     int num_bytes_read=0;
     char* buffer =  malloc(sizeof(char)*128);
@@ -13,18 +13,25 @@ void readLine(){
         if(c=='\n'){
             break;
         }
-        num_bytes_read+=sizeof(&c);
+        num_bytes_read+=sizeof(c);
     }
     free(buffer);
-    printf("%d %s",num_bytes_read);
+    printf("Number of bytes read: %d\n",num_bytes_read);
+    if(num_bytes_read==0){
+        return 1;
+    }else{
+        return 0;
+    }
+    
 }
 
 
 
 int main(int argc,char *argv[]){
     int status=1;
-    readLine();
+    
     do{
         printf(">");
-    }while(status);
+        status = readLine();
+    }while(!status);
 }
