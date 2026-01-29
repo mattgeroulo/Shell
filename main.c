26b9990edc845969a123f0367ee31aca4ec176f7
mattgeroulo <mattgeroulo@yahoo.com>
2026-01-28 19:39:32 -0500

    ls print current directory, input and flags next

--- main.c
+++ main.c
@@ -58,7 +58,7 @@ int main(int argc,char *argv[]){
             //fork to a child process, set child process fd to this process fd, execute ls
             pid_t pid = fork();
             //both processes run from here, use pid value to determine who is child and who is parent
-
+           
             if (pid== -1){
                 #if DEBUG
                     printf("Failed to fork from parent\n");
@@ -66,18 +66,22 @@ int main(int argc,char *argv[]){
             }
             else if (pid==0){
                 //CHILD PROCESS
+                #if DEBUG
+                    printf("Child process is: %u , Parent process is %u \n",getpid(),getppid());
+                #endif
                 char * args[2];
                 args[0]="/bin/ls";
                 args[1]=NULL;
                 execv(args[0],args);
                 #if DEBUG
-                    printf("Error on child process exec call");
+                    printf("Error on child process exec call\n");
                 #endif
                 exit(1);
             }else{
-                wait(NULL);
+                pid_t child_pid = wait(NULL);
                 #if DEBUG
-                    printf("Parent: child process finished,we back to shell");
+                    printf("child process reaped, id was : %u\n",child_pid);
+                    printf("Parent: child process finished,we back to shell\n");
                 #endif
             }
             //pid_t child_pid = getpid();
